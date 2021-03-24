11b00e64d56a374c91c905e759622396936c30c8
Kumar Vaibhav Jha <jha.kumarv@gmail.com>
2021-03-24 12:51:08 -0400

    New Commit 2

--- project/src/project/HelloWorld.java
+++ project/src/project/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[]args) {
 		System.out.println("Hello World");
 		System.out.println("My name is Kumar");
-		System.out.println("We tryna get Merge Conflicts");
+		System.out.println("We tryna get Merge Conflicts As Well");
 	}
 
 }
