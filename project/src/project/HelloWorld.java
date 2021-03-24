f011d2ce4cc433c65dce861b0a734f54dddc78b8
Kumar Vaibhav Jha <jha.kumarv@gmail.com>
2021-03-24 12:42:47 -0400

    Added some more lines

--- project/src/project/HelloWorld.java
+++ project/src/project/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[]args) {
 		System.out.println("Hello World");
 		System.out.println("My name is Kumar");
+		System.out.println("We tryna get Merge Conflicts");
 	}
 
 }
